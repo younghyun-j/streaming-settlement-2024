e110d8ceee08d9c79db47653833e0a1edf4147e7
younghyun.j <zerohyun.j@gmail.com>
2024-11-25 02:26:37 +0900

    [feat] project init, docker-compose, yml 파일 작성

---
+++ src/main/java/com/streaming/settlement/streamingsettlement/StreamingSettlementApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.settlement.streamingsettlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingSettlementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingSettlementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/streaming/settlement/streamingsettlement/StreamingSettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.settlement.streamingsettlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingSettlementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
